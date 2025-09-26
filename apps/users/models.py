5017dbf4a4d9927ed96bc25c7cf7a391b4bd3e52
angelchiav <achiavicuna@gmail.com>
2025-09-26 19:42:05 -0300

    Add custom User model with profile fields
    Introduced a custom User model extending AbstractUser, including fields for bio, date_of_birth, role, and profile_pic, along with validation and utility methods. Updated settings to use the new user model and added Pillow as a dependency for image handling.

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,63 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = [("A", "Administrator"), ("M", "Moderator"), ("U", "User")]
+    bio = models.TextField(
+        blank=True,
+        max_length=500,
+        help_text="Bio (500 char. maximum)",
+    )
+
+    date_of_birth = models.DateField(help_text="Obligatory.")
+
+    role = models.CharField(
+        max_length=1,
+        choices=ROLE_CHOICES,
+        default="U",
+    )
+
+    profile_pic = models.ImageField(
+        upload_to="profile-pics/",
+        null=True,
+        blank=True,
+        verbose_name="Profile picture",
+    )
+
+    class Meta:
+        ordering = ["first_name", "last_name"]
+
+    def get_age(self):
+        from datetime import date
+
+        today = date.today()
+        age = today.year - self.date_of_birth.year
+
+        if today.month < self.date_of_birth.month or (
+            today.month == self.date_of_birth.month
+            and today.day < self.date_of_birth.day
+        ):
+            age -= 1
+
+        return age
+
+    def clean(self):
+        from django.core.exceptions import ValidationError
+        from django.utils import timezone
+
+        if self.date_of_birth >= timezone.now().date():
+            raise ValidationError(
+                {"date_of_birth": "The date of birth is after this date."}
+            )
+
+        self.first_name.lower().capitalize()
+        self.last_name.lower().capitalize()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
+
+    def __repr__(self):
+        return f"""{self.first_name} {self.last_name}
+        {self.role} ({self.email})
+        IS ACTIVE: {self.is_active}"""
